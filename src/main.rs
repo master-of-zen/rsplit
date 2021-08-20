982db663384ae3d3ad38fbd02cb09ab027e853f4
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-20 19:03:25 +0300

    + Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap, Debug)]
+#[clap(version = "0.1", author = "Zen <master_of_zen@protonmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Args {
+    #[clap(short, long)]
+    input: String,
+
+    #[clap(short, long)]
+    size: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:#?}", args);
 }
