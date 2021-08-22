5716266953b78f2a5a7388e35f40bf985448f7f5
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-22 19:49:51 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::exit;
+use std::process::Command;
 
 use bytefmt;
 use clap::{AppSettings, Clap};
@@ -17,19 +18,25 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let size: u64 = match bytefmt::parse_to(&args.size, bytefmt::Unit::KIB) {
-        Ok(size) => size as u64,
-        Err(e) => {
-            println!(
-                "Can't parse byte format, valid sizes: [KB,KiB,MB,MiB,GB,GiB], \nProvided: {:#?} Example: 5 MiB\nerror: {:#?}",
-                &args.size,
-                e
-            );
-            exit(1);
-        }
-    };
-
+    let size: u64 = parse_to_bytes(&args.size);
     println!("{:#?}", args);
 
     println!("{:#?}", size)
+}
+
+fn segment() {}
+
+/// Takes string of size provided by user and returns u64 of bytes
+fn parse_to_bytes(size: &String) -> u64 {
+    match bytefmt::parse_to(size, bytefmt::Unit::KIB) {
+        Ok(size) => size as u64,
+        Err(e) => {
+            println!(
+            "Can't parse byte format, valid sizes: [KB,KiB,MB,MiB,GB,GiB], \nProvided: {:#?} Example: 5 MiB\nerror: {:#?}",
+            size,
+            e
+        );
+            exit(1);
+        }
+    }
 }
