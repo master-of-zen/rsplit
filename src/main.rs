df2ef54894d56ff3ee85e0a2a0f4deed353e4a0d
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-23 21:33:34 +0300

    get duration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use chrono::NaiveTime;
+use std::path::PathBuf;
 use std::process::exit;
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::str::FromStr;
+use std::time::Duration;
 
 use bytefmt;
 use clap::{AppSettings, Clap};
@@ -19,12 +23,13 @@ fn main() {
     let args = Args::parse();
 
     let size: u64 = parse_to_bytes(&args.size);
-    println!("{:#?}", args);
+    // println!("{:#?}", args);
+
+    let fl = PathBuf::from_str(&args.input).unwrap();
+    get_duration(fl);
 
     println!("{:#?}", size)
 }
-
-fn segment() {}
 
 /// Takes string of size provided by user and returns u64 of bytes
 fn parse_to_bytes(size: &String) -> u64 {
@@ -40,3 +45,45 @@ fn parse_to_bytes(size: &String) -> u64 {
         }
     }
 }
+
+/// Parses ffmpeg output string and returns duration in seconds and miliseconds
+fn _parse_duration(_ffmpeg_string: String) {
+    let duration = "00:00:11";
+    let fmt_str = "%H:%M:%S.%09m";
+    let no_timezone = NaiveTime::parse_from_str(&duration, fmt_str).unwrap();
+    dbg!(no_timezone);
+}
+
+fn get_duration(file: PathBuf) {
+    let mut cmd = Command::new("ffmpeg");
+    cmd.args(&[
+        "-hide_banner",
+        "-i",
+        file.as_os_str().to_owned().to_str().unwrap(),
+        "-f",
+        "null",
+        "-",
+    ]);
+
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+
+    let out = cmd.output();
+
+    dbg!(&out);
+    let output = match out {
+        Ok(st) => match st.status.success() {
+            true => String::from_utf8(st.stderr),
+            false => panic!(
+                "Failed to execute ffmpeg. \n{:#?}\n {:#?}",
+                String::from_utf8(st.stderr),
+                st.status.success()
+            ),
+        },
+        Err(st) => panic!("\nFailed to execute ffmpeg. \n{:#?}", st),
+    };
+
+    println!("{:#?}", output);
+}
+
+fn _segment() {}
