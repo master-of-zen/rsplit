35ff45c3b917c3104f483a5f6b05f42ffddf9095
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-24 21:33:52 +0300

    regex match time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::NaiveTime;
+use regex::{self, Regex};
 use std::path::PathBuf;
 use std::process::exit;
 use std::process::{Command, Stdio};
@@ -46,6 +47,24 @@ fn parse_to_bytes(size: &String) -> u64 {
     }
 }
 
+fn parse_ffmpeg_time(ffmpeg_string: String) {
+    let re = Regex::new(r"time=(\d+):(\d+):(\d+).(\d+)").unwrap();
+    let (hour, min, sec, mil) = match re.is_match(&ffmpeg_string) {
+        true => {
+            let re_match = re.captures_iter(&ffmpeg_string).last().unwrap();
+            (
+                re_match.get(1).unwrap().as_str(),
+                re_match.get(2).unwrap().as_str(),
+                re_match.get(3).unwrap().as_str(),
+                re_match.get(4).unwrap().as_str(),
+            )
+        }
+        false => panic!("\nFailed to match regex for:\n{:#?}", ffmpeg_string),
+    };
+    dbg!(hour, min, sec, mil);
+    //ffmpeg_string
+}
+
 /// Parses ffmpeg output string and returns duration in seconds and miliseconds
 fn _parse_duration(_ffmpeg_string: String) {
     let duration = "00:00:11";
@@ -70,10 +89,9 @@ fn get_duration(file: PathBuf) {
 
     let out = cmd.output();
 
-    dbg!(&out);
     let output = match out {
         Ok(st) => match st.status.success() {
-            true => String::from_utf8(st.stderr),
+            true => String::from_utf8(st.stderr).unwrap(),
             false => panic!(
                 "Failed to execute ffmpeg. \n{:#?}\n {:#?}",
                 String::from_utf8(st.stderr),
@@ -83,7 +101,8 @@ fn get_duration(file: PathBuf) {
         Err(st) => panic!("\nFailed to execute ffmpeg. \n{:#?}", st),
     };
 
-    println!("{:#?}", output);
+    let duration = parse_ffmpeg_time(output);
+    println!("{:#?}", duration);
 }
 
 fn _segment() {}
