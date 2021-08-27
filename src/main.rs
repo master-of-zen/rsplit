6c0e36541195a575691902b3edfb4dcadc00e122
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-08-27 23:54:02 +0300

    Working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use bytefmt;
-use chrono::NaiveTime;
 use clap::{AppSettings, Clap};
 use regex::{self, Regex};
+use std::fs;
 use std::path::PathBuf;
 use std::process::exit;
 use std::process::{Command, Stdio};
 use std::str::FromStr;
-use std::time::Duration;
 
 #[derive(Clap, Debug)]
 #[clap(version = "0.1", author = "Zen <master_of_zen@protonmail.com>")]
@@ -22,20 +21,25 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let input_size: u64 = parse_to_bytes(&args.size);
+    let split_size: u64 = parse_to_bytes(&args.size);
     let fl = PathBuf::from_str(&args.input).unwrap();
+    let input_duration = get_duration(&fl);
+    let input_size = fs::metadata(&fl).unwrap().len();
+    let segments = input_size / split_size;
 
-    let input_duration = get_duration(fl);
+    fl.canonicalize().unwrap();
 
-    // println!("{:#?}", args);
-
-    println!("Input size: {}", input_size);
     println!("Input length: {}", format_time(input_duration));
+    println!("Input size: {}B", input_size);
+    println!("Split size: {}B", split_size);
+    println!("Expected segments: {}", segments);
+
+    segmenting(fl, input_duration, split_size)
 }
 
 /// Takes string of size provided by user and returns u64 of bytes
 fn parse_to_bytes(size: &String) -> u64 {
-    match bytefmt::parse_to(size, bytefmt::Unit::KIB) {
+    match bytefmt::parse_to(size, bytefmt::Unit::B) {
         Ok(size) => size as u64,
         Err(e) => {
             println!(
@@ -63,12 +67,11 @@ fn parse_ffmpeg_time(ffmpeg_string: String) -> u64 {
         }
         false => panic!("\nFailed to match regex for:\n{:#?}", ffmpeg_string),
     };
-    dbg!(hour, min, sec, mil);
 
     hour * 60 * 60 * 1000 + min * 60 * 1000 + sec * 1000 + mil
 }
 /// Gets duration of file in miliseconds
-fn get_duration(file: PathBuf) -> u64 {
+fn get_duration(file: &PathBuf) -> u64 {
     let mut cmd = Command::new("ffmpeg");
     cmd.args(&[
         "-hide_banner",
@@ -99,14 +102,80 @@ fn get_duration(file: PathBuf) -> u64 {
     parse_ffmpeg_time(output)
 }
 
-fn _segment() {}
+/// Segmeting input file in while loop until it's exhausted
+fn segmenting(input_file: PathBuf, input_duration: u64, split_size: u64) {
+    let mut done_length = 0u64;
+    let end_length = input_duration;
 
-fn format_time(input_duration: u64) -> String {
+    let mut segments = 0u64;
+    while done_length < end_length {
+        segments += 1;
+
+        let output = &input_file.clone().with_file_name(format!(
+            "{}_{}.{}",
+            input_file.file_stem().unwrap().to_str().unwrap(),
+            segments,
+            input_file.extension().unwrap().to_str().unwrap(),
+        ));
+
+        segment(&input_file, &output, done_length, split_size);
+
+        let chunk_length = get_duration(&output);
+        let chunk_size = fs::metadata(output).unwrap().len();
+        println!(
+            "Chunk: {} Size: {}, Length: {} ",
+            segments, chunk_size, chunk_length
+        );
+
+        done_length += chunk_length;
+    }
+}
+
+fn segment(input_file: &PathBuf, output: &PathBuf, start: u64, split_size: u64) {
+    let ffmpeg_ss = format_time(start);
+
+    let mut cmd = Command::new("ffmpeg");
+
+    cmd.args([
+        "-hide_banner",
+        "-y",
+        "-ss",
+        &ffmpeg_ss,
+        "-i",
+        input_file.as_os_str().to_str().unwrap(),
+        "-fs",
+        &split_size.to_string(),
+        "-c",
+        "copy",
+        output.as_os_str().to_str().unwrap(),
+    ]);
+
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+
+    let out = cmd.output();
+
+    match out {
+        Ok(st) => match st.status.success() {
+            true => return,
+            false => panic!(
+                "Failed to execute ffmpeg. \n{:#?}\n {:#?}",
+                String::from_utf8(st.stderr),
+                st.status.success()
+            ),
+        },
+        Err(st) => panic!("\nFailed to execute ffmpeg. \n{:#?}", st),
+    };
+}
+
+/// Formats time to HH:MM:SS.m
+pub fn format_time(input_duration: u64) -> String {
+    let hours = input_duration / 60 / 60 / 1000;
+    let minutes = input_duration % 3600000 / 60 / 1000;
+    let seconds = input_duration % 60000 / 1000;
+    let miliseconds = input_duration % 1000;
     format!(
         "{:.0}:{:.0}:{:.0}.{:.0}",
-        input_duration / 60 / 60 / 1000,
-        input_duration / 60 / 1000,
-        input_duration / 1000,
-        input_duration % 1000
+        hours, minutes, seconds, miliseconds
     )
 }
